6617209e05e6c8808bbde181e0dc63fa6a4b47d3
SeanCCarter <seancarter26@gmail.com>
2016-04-05 16:08:56 -0400

    Added database models.

---
+++ models/printModel.js
@@ -0,0 +1,32 @@
+/* Schema for each user
+ * name: Name of person
+ * email: email of person
+ * purpose: Class/Scope, Research, Team, etc...
+ * classes: What class is it for?
+ * printMass: mass of print, in grams
+ * dateAndTime: what it says
+ * durration: time it takes to print
+ * ninjaApproval: if over six hours, has it been approved?
+ * printer: which printer is being used
+ * problems: any issues that cropped up durring printing
+ */
+
+var mongoose = require('mongoose');
+
+// Create a wiki page schema
+var printSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    part: String,
+    purpose: String,
+    classes: String,
+    printMass: Number,
+    dateAndTime: String,
+    durration: Number, //In minutes
+    ninjaApproval: Boolean,
+    printer: String,
+    problems: String
+
+});
+
+module.exports  = mongoose.model('print', printSchema);
