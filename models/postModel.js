ab2a0c83260febac341e0156992325c9759b40e6
buttegab <buttegab@users.noreply.github.com>
2016-04-12 15:38:08 -0400

    Delete postModel.js

--- models/postModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-// Create a wiki page schema
-var postSchema = mongoose.Schema({
-    user: String,
-    content: String
-});
-
-module.exports  = mongoose.model('post', postSchema);
