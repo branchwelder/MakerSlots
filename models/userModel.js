5f1d6729cd3f6b3de274b4f9a89f50ceeef15e3e
buttegab <gabriel.butterick@students.olin.edu>
2016-04-12 15:32:07 -0400

    fix

--- models/userModel.js
+++ models/userModel.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 // Create a wiki page schema
 var userSchema = mongoose.Schema({
     name: String,
+    password: String,
     printHist: String,
     forumHist: String,
     isNinja: Boolean,
