12aae979d74478ed274464b1bcc1ad5cbbd40552
buttegab <buttegab@users.noreply.github.com>
2016-05-02 23:02:22 -0400

    Update home.js

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,25 +1,17 @@
-console.log("loading home.js");
 var onSuccess = function(data, status) {
   console.log('added event');
   window.location = "/schedule";
-
-
 };
   
-  // $("#result").append("<div id='result'>"+data.name+": "+data.price+"$</div>");
-
-
 var onError = function(data, status) {
   console.log("status", status);
   console.log("error", data);
   $("#error").replaceWith('<div id="error">*Either a required field has been left empty or you have attempted to submit a 6+ hour print without ninja approval*</div>')
 };
 
-//i want this to, on click, ask the user if they are trying to edit or delete a print
-// $form1.submit(function(event) {
+//Handles grabbing all the form information and proccessing it when needbe
 $("#printForm").submit(function(event) {
   event.preventDefault();
-  // var purposes = $("input[name|=purpose]")
   var purposes = $('input[name=purpose]:checked').val();
   var name = $("#name").val();
   var email = $("#email").val();
@@ -35,8 +27,6 @@ $("#printForm").submit(function(event) {
   var printer = $("input[name=printer]:checked").val();
   var duration = parseInt(hoursDur)*60 + parseInt(minutesDur);
   var problems = $("#problems").val();
-
-  //var name = $form.find("[name='name']").val();
 
   $.get("add", {
     purpose: purposes,
