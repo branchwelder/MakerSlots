bb20eca2f795a016c70a95e7f1644e25c479c23d
buttegab <buttegab@users.noreply.github.com>
2016-05-02 23:08:36 -0400

    Update main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,32 +1,28 @@
 //populates calendar, autofills form for edits
 var onSuccess = function(data, status) {
-  // window.location = "/editForm";
-  console.log("data printed below");
-  console.log(data);
-  console.log("data printed above");
   $(document).ready(function(){
-    console.log("document readied");
     $('input[value='+data.purpose+']').prop('checked',true); 
     $("#name").replaceWith('<input type="text" class="form-control" id="name" value="'+data.name+'">');
     $("#email").replaceWith('<input type="text" class="form-control" id="email" value="'+data.email+'">');
     $("#part").replaceWith('<input type="text" class="form-control" id="part" value="'+data.part+'">');
     $("#whatclass").replaceWith('<input type="text" class="form-control" id="whatclass" value="'+data.classes+'">');
     $("#mass").replaceWith('<input type="text" class="form-control" id="mass" value='+data.printMass+'>');
-    
     $("#whenprint").replaceWith('<input type="text" class="form-control" id="whenprint" value="'+data.dateAndTime+'">');
     $("#whendone").replaceWith('<input type="text" class="form-control" id="whendone" value="'+data.finish+'">');
+    
     var hours = Math.floor(data.duration/60);
     var minutes = data.duration%60;
+    
     $("input[name=hours]").replaceWith('<input type="number" class="form-control numberentry" name="hours" min="0" max="50" value="'+hours+'">')
     $("input[name=minutes]").replaceWith('<input class="form-control numberentry" type="number" name="minutes" min="0" max="59" value="'+minutes+'">')
     
+    //change the data into something the program can use to find the html elements
     if(data.ninjaApproval == true) {
       var currentBoolean = "yes";
     }
     else {
       var currentBoolean = "no";
     }
-
 
     $("input[value="+currentBoolean+"]").prop('checked',true);
     $('input[value='+data.printer+']').prop('checked',true);
@@ -38,32 +34,23 @@ var onSuccess2 = function(data, status) {
   for(var i = 0; i<data.length; i++){
     $('.fc').fullCalendar( 'renderEvent', data[i], true )  // Add Event to fullCalendar
   };
-  
-  // $("#result").append("<div id='result'>"+data.name+": "+data.price+"$</div>");
 };
 
+//change window location to schedule page after submitting a print
 var onSuccess3 = function(data, status) {
-  console.log("success3 ran");
   window.location = "/schedule";
-
-  
-  // $("#result").append("<div id='result'>"+data.name+": "+data.price+"$</div>");
 };
 
+//change window location when deleting a print
 var onSuccess4 = function(data, status) {
-  console.log("success4 ran");
   window.location = "/schedule";
-
-  // $("#result").append("<div id='result'>"+data.name+": "+data.price+"$</div>");
 };
 
 var onError = function(data, status) {
   console.log("status", status);
-  console.log("error", data);
-  console.log("shits broke yo");
 };
-//i need to add an update to calendar entries in the calendar app to include an id, which will be its mongo id, which can be grabbed by clicking
-//on it and used to populate the form, allowing for editing.
+
+//creates calendar object and populates it with known prints
 $(document).ready(function() {
   clicked = undefined
     $('#calendar').fullCalendar({
@@ -89,18 +76,17 @@ $(document).ready(function() {
     calendar.on('dayClick', function(date, jsEvent, view) {
       alert('clicked on ' + date.format());
     });
-// this part on needs to be in success function
-    
+
   $.get("prints", {
   })
     .done(onSuccess2)
     .error(onError);
 });
 
+//Handles the new, edit information for the form and passes it to the database
 $("#printFormEdit").submit(function(event) {
   if(clicked != undefined) {
     event.preventDefault();
-    // var purposes = $("input[name|=purpose]")
     var purposes = $('input[name=purpose]:checked').val();
     var name = $("#name").val();
     var email = $("#email").val();
@@ -116,8 +102,6 @@ $("#printFormEdit").submit(function(event) {
     var printer = $("input[name=printer]:checked").val();
     var duration = parseInt(hoursDur)*60 + parseInt(minutesDur);
     var problems = $("#problems").val();
-
-    //var name = $form.find("[name='name']").val();
 
     $.get("editPrint", {
       id: clicked,
@@ -140,6 +124,7 @@ $("#printFormEdit").submit(function(event) {
 }
 });
 
+//Deletes a print after prompting
 function delPrint() {
   var r = confirm("Are you sure you want to delete this print?");
   if (r == true) {
@@ -150,16 +135,3 @@ function delPrint() {
     }   
   } 
 }
-
-// $('#calendar').fullCalendar({
-//     eventClick: function(calEvent, jsEvent, view) {
-
-//         alert('Event: ' + calEvent.title);
-//         alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-//         alert('View: ' + view.name);
-
-//         // change the border color just for fun
-//         $(this).css('border-color', 'red');
-
-//     }
-// });
